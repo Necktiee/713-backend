fb48b2c6fb771fb9fd218beb6c943c71992c12f7
Teeraphat Raksawong <teeraphat_ra@cmu.ac.th>
2025-03-15 14:25:51 +0700

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import express, { Request, Response } from "express";
 import multer from 'multer';
 import dotenv from 'dotenv';
@@ -8,7 +7,7 @@ dotenv.config();
 
 import { uploadFile } from './services/uploadFileService';
 const app = express();
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://713-2024-frontend.vercel.app'];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins
@@ -19,9 +18,6 @@ app.use(cors(options));
 app.use(express.json());
 app.use('/events',eventRoute);
 const port = process.env.PORT || 3000;
-
-
-
 
 const upload = multer({ storage: multer.memoryStorage() });
 
@@ -34,17 +30,3 @@ app.post('/upload', upload.single('file'), async (req: any, res: any) => {
 
     const bucket = process.env.SUPABASE_BUCKET_NAME;
     const filePath = process.env.UPLOAD_DIR;
-
-    if (!bucket || !filePath) {
-      return res.status(500).send('Bucket name or file path not configured.');
-    }
-    const ouputUrl = await uploadFile(bucket, filePath, file);
-
-    res.status(200).send(ouputUrl);
-  } catch (error) {
-    res.status(500).send('Error uploading file.');
-  }
-});
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
